4a4ce2a256627ff7f8951e5fb6785521aff96cb7
Rolandas Razma <rolandas@razma.lt>
2012-07-16 13:59:13 +0100

    AI can play black or white

--- RRGame/Classes/Model/RRAIPlayer.m
+++ RRGame/Classes/Model/RRAIPlayer.m
@@ -19,102 +19,90 @@
 
 
 - (RRTileMove)bestMoveOnBoard:(RRGameBoardLayer *)gameBoard {
-    RRTileMove tileMove;
+    RRTileMove tileMove = RRTileMoveZero;
     CGFloat tileMoveScore = 0.0f;
     
     RRTile *activeTile = gameBoard.activeTile;
     
-    
-    
-    NSUInteger white, black;
+
     
 
     for ( RRTile *tile in gameBoard.children ) {
         if( [tile isEqual:activeTile] ) continue;
         
         CGPoint positionInGrid = tile.positionInGrid;
-        
-        // TODO: padaryti tikrinima ar nenusuka savo spalvos uz ekrano ribu arba i siena
-        
+
         if( [gameBoard canPlaceTileAtGridLocation:CGPointMake(positionInGrid.x +1, positionInGrid.y)] ){
             NSLog(@"x +1");
             
-            [activeTile setPositionInGrid:CGPointMake(positionInGrid.x +1, positionInGrid.y)];
-            
-            for( NSUInteger angle=0; angle<=270; angle += 90 ){
-                [activeTile setRotation:angle];
-                [gameBoard countSymbolsAtTile:activeTile white:&white black:&black];
-                
-                CGFloat moveValue = (float)black -white;
-                
-                if( moveValue >= tileMoveScore ){
-                    tileMoveScore    = moveValue;
-                    tileMove.rotation = angle;
-                    tileMove.positionInGrid = activeTile.positionInGrid;
-                }
+            RRTileMove bestMove = [self bestMoveOnGameBoard:gameBoard positionInGrid:CGPointMake(positionInGrid.x +1, positionInGrid.y)];
+            if( bestMove.score >= tileMove.score ){
+                tileMove = bestMove;
             }
         }
         
         if( [gameBoard canPlaceTileAtGridLocation:CGPointMake(positionInGrid.x -1, positionInGrid.y)] ){
             NSLog(@"x -1");
             
-            [activeTile setPositionInGrid:CGPointMake(positionInGrid.x -1, positionInGrid.y)];
-            
-            for( NSUInteger angle=0; angle<=270; angle += 90 ){
-                [activeTile setRotation:angle];
-                [gameBoard countSymbolsAtTile:activeTile white:&white black:&black];
-                
-                CGFloat moveValue = (float)black -white;
-                
-                if( moveValue >= tileMoveScore ){
-                    tileMoveScore    = moveValue;
-                    tileMove.rotation = angle;
-                    tileMove.positionInGrid = activeTile.positionInGrid;
-                }
+            RRTileMove bestMove = [self bestMoveOnGameBoard:gameBoard positionInGrid:CGPointMake(positionInGrid.x -1, positionInGrid.y)];
+            if( bestMove.score >= tileMove.score ){
+                tileMove = bestMove;
             }
         }
         
         if( [gameBoard canPlaceTileAtGridLocation:CGPointMake(positionInGrid.x, positionInGrid.y +1)] ){
             NSLog(@"y +1");
-            
-            [activeTile setPositionInGrid:CGPointMake(positionInGrid.x, positionInGrid.y +1)];
-            
-            for( NSUInteger angle=0; angle<=270; angle += 90 ){
-                [activeTile setRotation:angle];
-                [gameBoard countSymbolsAtTile:activeTile white:&white black:&black];
-                
-                CGFloat moveValue = (float)black -white;
-                
-                if( moveValue >= tileMoveScore ){
-                    tileMoveScore    = moveValue;
-                    tileMove.rotation = angle;
-                    tileMove.positionInGrid = activeTile.positionInGrid;
-                }
+
+            RRTileMove bestMove = [self bestMoveOnGameBoard:gameBoard positionInGrid:CGPointMake(positionInGrid.x, positionInGrid.y +1)];
+            if( bestMove.score >= tileMove.score ){
+                tileMove = bestMove;
             }
         }
         
         if( [gameBoard canPlaceTileAtGridLocation:CGPointMake(positionInGrid.x, positionInGrid.y -1)] ){
             NSLog(@"y -1");
             
-            [activeTile setPositionInGrid:CGPointMake(positionInGrid.x, positionInGrid.y -1)];
-            
-            for( NSUInteger angle=0; angle<=270; angle += 90 ){
-                [activeTile setRotation:angle];
-                [gameBoard countSymbolsAtTile:activeTile white:&white black:&black];
-                
-                CGFloat moveValue = (float)black -white;
-                
-                if( moveValue >= tileMoveScore ){
-                    tileMoveScore    = moveValue;
-                    tileMove.rotation = angle;
-                    tileMove.positionInGrid = activeTile.positionInGrid;
-                }
+            RRTileMove bestMove = [self bestMoveOnGameBoard:gameBoard positionInGrid:CGPointMake(positionInGrid.x, positionInGrid.y -1)];
+            if( bestMove.score >= tileMove.score ){
+                tileMove = bestMove;
             }
         }
         
     }
     
     NSLog(@"!move to: {%f, %f} at angle: %.f confidence: %.2f", tileMove.positionInGrid.x, tileMove.positionInGrid.y, tileMove.rotation, tileMoveScore);
+    
+    return tileMove;
+}
+
+
+- (RRTileMove)bestMoveOnGameBoard:(RRGameBoardLayer *)gameBoard positionInGrid:(CGPoint)positionInGrid {
+    RRTileMove tileMove = RRTileMoveZero;
+    RRTile *activeTile = gameBoard.activeTile;
+    
+    NSUInteger white, black;
+    
+    [activeTile setPositionInGrid:positionInGrid];
+    
+    // TODO: padaryti tikrinima ar nenusuka savo spalvos uz ekrano ribu arba i siena
+    
+    for( NSUInteger angle=0; angle<=270; angle += 90 ){
+        [activeTile setRotation:angle];
+        [gameBoard countSymbolsAtTile:activeTile white:&white black:&black];
+        
+        CGFloat moveValue;
+        if( self.playerColor == RRPlayerColorBlack ){
+            moveValue = (float)black -white;
+        }else if( self.playerColor == RRPlayerColorWhite ){
+            moveValue = (float)white -black;
+        }
+        
+        if( moveValue >= tileMove.score ){
+            tileMove.score    = moveValue;
+            tileMove.rotation = angle;
+            tileMove.positionInGrid = activeTile.positionInGrid;
+        }
+    }
     
     return tileMove;
 }
