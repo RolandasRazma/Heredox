2000f6598a7c98b86bb6b4f112d198cd00c1ce45
Rolandas Razma <rolandas@razma.lt>
2012-07-25 19:48:53 +0100

    up

--- RRGame/Classes/Scenes/RRDifficultyScene/RRDifficultyLayer.m
+++ RRGame/Classes/Scenes/RRDifficultyScene/RRDifficultyLayer.m
@@ -9,11 +9,16 @@
 #import "RRDifficultyLayer.h"
 #import "UDSpriteButton.h"
 #import "RRPickColorScene.h"
+#import "RRAIPlayer.h"
 
 
 @implementation RRDifficultyLayer {
     RRGameMode      _gameMode;
     RRPlayerColor   _playerColor;
+    
+    UDSpriteButton  *_buttonNovice;
+    UDSpriteButton  *_buttonDeacon;
+    UDSpriteButton  *_buttonAbbot;
 }
 
 
@@ -44,9 +49,31 @@
         [buttonHome addBlock: ^{ [self showMenu]; } forControlEvents: UDButtonEventTouchUpInside];
         [self addChild:buttonHome];
      
+        
+        // Dificulty buttons
+        _buttonNovice = [UDSpriteButton buttonWithSpriteFrameName:@"RRButtonAILevelNovice.png" highliteSpriteFrameName:@"RRButtonAILevelNoviceSelected.png"];
+        [_buttonNovice addBlock: ^{ [self setDificultyLevel:RRAILevelNovice]; } forControlEvents: UDButtonEventTouchUpInside];
+        [self addChild:_buttonNovice];
+        
+        _buttonDeacon = [UDSpriteButton buttonWithSpriteFrameName:@"RRButtonAILevelDeacon.png" highliteSpriteFrameName:@"RRButtonAILevelDeaconSelected.png"];
+        [_buttonDeacon addBlock: ^{ [self setDificultyLevel:RRAILevelDeacon]; } forControlEvents: UDButtonEventTouchUpInside];
+        [self addChild:_buttonDeacon];
+        
+        _buttonAbbot = [UDSpriteButton buttonWithSpriteFrameName:@"RRButtonAILevelAbbot.png" highliteSpriteFrameName:@"RRButtonAILevelAbbotSelected.png"];
+        [_buttonAbbot addBlock: ^{ [self setDificultyLevel:RRAILevelAbbot]; } forControlEvents: UDButtonEventTouchUpInside];
+        [self addChild:_buttonAbbot];
+        
+        [self setDificultyLevel: [[NSUserDefaults standardUserDefaults] integerForKey:@"RRHeredoxAILevel"]];
+        
+        
+
         // Device layout
         if( isDeviceIPad() ){
             [buttonHome setPosition:CGPointMake(winSize.width -15, winSize.height -15)];
+            
+            [_buttonNovice setPosition:CGPointMake(180, 460)];
+            [_buttonDeacon setPosition:CGPointMake(winSize.width /2, 445)];
+            [_buttonAbbot setPosition:CGPointMake(620, 445)];
         }else{
             
         }
@@ -56,11 +83,21 @@
 
 
 - (void)showMenu {
-    
+
     RRPickColorScene *pickColorScene = [[RRPickColorScene alloc] initWithNumberOfPlayers:1];
 	[[CCDirector sharedDirector] replaceScene: [CCTransitionPageTurn transitionWithDuration:0.7f scene:pickColorScene backwards:YES]];
     [pickColorScene release];
+
+}
+
+
+- (void)setDificultyLevel:(RRAILevel)dificultyLevel {
     
+    [_buttonNovice setSelected:(dificultyLevel==RRAILevelNovice)];
+    [_buttonDeacon setSelected:(dificultyLevel==RRAILevelDeacon)];
+    [_buttonAbbot setSelected:(dificultyLevel==RRAILevelAbbot)];
+    
+    [[NSUserDefaults standardUserDefaults] setInteger:dificultyLevel forKey:@"RRHeredoxAILevel"];
 }
 
 
